6946050fb6024ad0ac05b1764ff5dd461b2d3b71
sulayam <sulayam@gmail.com>
2025-05-04 07:22:16 +0400

    basic crud code has been done in this commit

--- blog/main.py
+++ blog/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, status, Response, HTTPException
 from . import schemas, models
 from . database import engine, SessionLocal
 from sqlalchemy.orm import Session
@@ -14,6 +14,42 @@ def get_db():
     finally:
         db.close()
 
-@app.post("/blog")
-def create_blog(request: schemas.Blog, db:Session = Depends(get_db)):
-    return db
+@app.post("/blog", status_code=status.HTTP_201_CREATED)
+def create(request: schemas.Blog, db:Session = Depends(get_db)):
+    new_blog = models.Blog(title=request.title, body=request.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
+
+@app.get('/blog')
+def all_blogs(db: Session= Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs
+
+@app.get("/blog/{id}", status_code=200)
+def show(id: int, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} is not available")
+    return blog
+
+@app.delete("/blog/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def destroy(id: int, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} is not available")
+
+    blog.delete(synchronize_session=False)
+    db.commit()
+    return f'deleted blog with id {id}'
+
+@app.put("/blog/{id}", status_code=status.HTTP_202_ACCEPTED)
+def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} is not available")
+    
+    blog.update(request.dict())
+    db.commit()
+    return 'updated successfully'
